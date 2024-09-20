df27fddb05c65980d91eefa2049b6730d96b9003
cyrxdzj <2824671438@qq.com>
2024-09-20 18:17:32 +0800

    新增down文件复制器。

---
+++ std/down_copy.py
@@ -0,0 +1,3 @@
+import shutil
+shutil.copy("../std/permutation/permutation_testlib_checker.exe","../down/permutation/checker.exe")
+shutil.copy("../std/library/library_probability_calc.cpp","../down/library/library_probability_calc.cpp")
