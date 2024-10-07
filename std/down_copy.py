907f0e6a72ce4750e67efde904ad87b077ff0836
cyrxdzj <2824671438@qq.com>
2024-10-07 19:54:36 +0800

    使用下发源文件的方式下发checker

--- std/down_copy.py
+++ std/down_copy.py
@@ -1,3 +1,4 @@
 import shutil
-shutil.copy("../std/permutation/permutation_testlib_checker.exe","../down/permutation/checker.exe")
+shutil.copy("../std/permutation/permutation_testlib_checker.cpp","../down/permutation/checker.cpp")
+shutil.copy("../std/permutation/testlib.h","../down/permutation/testlib.h")
 shutil.copy("../std/library/library_probability_calc.cpp","../down/library/library_probability_calc.cpp")
