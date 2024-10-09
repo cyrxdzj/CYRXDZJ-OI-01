47aa4951cbdacdbeb2dd094fa10c71c49c471f9e
cyrxdzj <2824671438@qq.com>
2024-10-09 22:19:56 +0800

    更改脚本配色，删除checker中的check_input

--- std/tree/tree_testlib_checker.cpp
+++ std/tree/tree_testlib_checker.cpp
@@ -4,34 +4,11 @@ using namespace std;
 
 const int MAXN=1e5;
 int n,m,csum;
-void check_input()
-{
-	n=inf.readInt(1,MAXN);
-	for(int i=1;i<n;i++)
-	{
-		int u=inf.readInt(1,n),v=inf.readInt(1,n);
-		if(u==v)
-		{
-			quitf(_fail,"edge %d u==v %d",i,u);
-		}
-	}
-	for(int i=1;i<=n;i++)
-	{
-		int a=inf.readInt(1,1000000000);
-	}
-	for(int i=1;i<=n;i++)
-	{
-		int b=inf.readInt(0,64-1);
-	}
-	int c=inf.readInt(0,64-1);
-}
 
 int main(int argc, char *argv[]) {
     setName("compare ordered sequences of signed int%d numbers", 8 * int(sizeof(long long)));
 
     registerTestlibCmd(argc, argv);
-    
-    check_input();
 
     int n = 0;
     string firstElems;
